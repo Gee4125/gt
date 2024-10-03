d54624d6ac4d2c1e3efee4e161645d55b1576a0f
Gee4125 <geetha4125@gmail.com>
2024-10-03 21:50:42 +0530

    Update middleware.tsx

--- middleware.tsx
+++ middleware.tsx
@@ -1,20 +1,14 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// Export the middleware function
+// Correctly export the middleware function
 const middleware = clerkMiddleware();
 
 export default middleware;
 
-// Configure the paths to apply middleware
 export const config = {
   matcher: [
-    // Exclude Next.js internals and static files (e.g., CSS, JS, images)
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    
-    // Apply middleware to all routes except SignIn and SignUp pages
     '/((?!Signin|Signup).*)',
-    
-    // Apply middleware to all API routes
     '/(api|trpc)(.*)',
   ],
 };
